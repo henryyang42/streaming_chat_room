44ed998027e79cc3fc92084912d1fa17b17580e1
henryyang42 <henryyang42@gmail.com>
2015-06-26 01:38:21 +0800

    finish basic feature

---
+++ chat/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib import admin
+from django.template.defaultfilters import slugify
+
+# Create your models here.
+
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    stream_src = models.CharField(max_length=128)
+    slug = models.SlugField(blank=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("room", (self.slug,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(ChatRoom, self).save(*args, **kwargs)
+
+
+class ChatUser(models.Model):
+    name = models.CharField(max_length=20)
+    session = models.CharField(max_length=20)
+    room = models.ForeignKey("chat.ChatRoom", related_name="users")
+
+    class Meta:
+        ordering = ("name",)
+
+    def __unicode__(self):
+        return self.name
+
+admin.site.register(ChatRoom)
+admin.site.register(ChatUser)
